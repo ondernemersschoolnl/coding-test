f2c11b798aed1054d39412b86dd12fb1299976f3
s124458-ap <s124458-@ap.be>
2026-06-24 12:11:56 +0200

    parse auth log to database

--- AuthResultaten.py
+++ AuthResultaten.py
@@ -5,3 +5,28 @@ DatabaseConnection = sqlite3.connect("AuthLog.db")
 DatabaseCutsor = DatabaseConnection.cursor()
 DatabaseCutsor.execute("CREATE TABLE IF NOT EXISTS AuthLogs(Timestamp, SwitchHost, SwitchPort, SwitchMac, user, Vlan, Result, Reason)")
 #res = DatabaseCutsor.execute("SELECT name FROM sqlite_master")
+#parsing auth-log.txt to database
+with open("auth_log.txt") as AuthBestand:
+  for AuthRecord in AuthBestand:
+    #print(AuthRecord)
+    #zin = AuthBestand.readline()
+    #zin = AuthBestand.readline()
+    #zin = AuthBestand.readline()
+    RecordValues = AuthRecord.split()
+    RecordLength = len(RecordValues)
+    RecordTulple = tuple(RecordValues)
+    #print(RecordTulple)
+    #print(RecordLength)
+    if RecordLength == 7:
+    #    print("Has no reson field")
+        RecordValues.append("accepted")
+        RecordTulple = tuple(RecordValues)
+        #print(RecordTulple)
+        DatabaseCutsor.execute("INSERT INTO AuthLogs VALUES (:Timestamp, :SwitchHost, :SwitchPort, :SwitchMac, :user, :Vlan, :Result, :Reason)", RecordTulple)
+
+    elif RecordLength == 8:
+    #   print has reason field
+        DatabaseCutsor.execute("INSERT INTO AuthLogs VALUES (:Timestamp, :SwitchHost, :SwitchPort, :SwitchMac, :user, :Vlan, :Result, :Reason)", RecordTulple)
+#result = DatabaseCutsor.execute("SELECT * FROM AuthLogs")
+#print(result.fetchall())
+
